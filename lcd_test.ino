77e728c50c3d7ebf3ad396df373d910bdfe3d883
adamwhurd <43097560+adamwhurd@users.noreply.github.com>
2021-04-18 20:39:09 -0700

    Add files via upload
    Added functionality for the temperature sensor. Temperature is now taken every second and displayed on the LCD.

--- lcd_test.ino
+++ lcd_test.ino
@@ -18,36 +18,40 @@
  *  A: 330 ohm resistor, then to +5v - this is the anode
  *  K: ground - this is the cathode
  *  
+ *  Temp sensor:
+ *  left pin: goes to arduino board pin (I chose A7)
+ *  Middle pin: +5v
+ *  Right pin: ground
  *  
  *  Note that the register select, enable, and data pins can be set to other pin locations. Just be sure to make the variable 
  *  assignments match their locations.
  *  
  *  
  */
-
-
-
+#include "DHT.h"
 #include <LiquidCrystal.h>
+
+#define DHT11_PIN A7  // set this value to whatever board pin your DHT data line is plugged into
+DHT tempSensor(DHT11_PIN, DHT11);
 
 // initialize the library by associating any needed LCD interface pin
 // with the arduino pin number it is connected to
 const int reg_sel = 12, en = 11, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
+
+float temperature = 0.0;
 LiquidCrystal lcd(reg_sel, en, d4, d5, d6, d7);
 
 void setup() 
 {
   // set up the LCD's number of columns and rows:
   lcd.begin(16, 2);
-  // Here's how we can display stuff
-  int temp = 70;
-  lcd.print("Temp: ");
-  lcd.print(temp);
-  lcd.print(" F");
+
+  tempSensor.begin(); // start collecting temperature data
   
-  // set the cursor to column 0, line 1
-  // (note: line 1 is the second row, since counting begins with 0):
+  displayTemperature(); // displays current temperature to the top line of the LCD
+  
+  // set the cursor to column 0, line 1(note: line 1 is the second row, since counting begins with 0):
   lcd.setCursor(0, 1);
-  
   
   // print the number of seconds since reset:
   char handPos = 'N';
@@ -57,5 +61,16 @@ void setup()
 
 void loop()
 {
+  displayTemperature();
   delay(1000);
+}
+
+void displayTemperature()
+{ // displays current temperature to the top line of the LCDv
+  lcd.setCursor(0, 0);  // set cursor to column 0, row 0 (leftmost bit of top row)
+  temperature = tempSensor.readTemperature(true); // This will read temperature as Fahrenheit. To read temp as Celsius, remove the "true" argument
+
+  lcd.print("Temp: ");
+  lcd.print(temperature);
+  lcd.print(" F");  // or "C" if temp is displayed in Celsius
 }
